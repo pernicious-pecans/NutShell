0997070af127c95d12adb0c1526dd39ece4b83de
Ryan Dillinger <rd.nss.services@gmail.com>
2019-02-21 10:55:53 -0600

    pushin

--- src/scripts/NavBar/navbarPopulate.js
+++ src/scripts/NavBar/navbarPopulate.js
@@ -1,3 +1,8 @@
 import navbars from "./navbar"
 import navEventListeners from "./navbarListeners"
+const $ = document.querySelector.bind(document)
 
+navbarCreation = () => {
+    $("#navbarSection").innerHTML = navbars.navbarGeneral()
+    
+}
